81fdd43053ce0c80b33ac6f3cc6a260c5009f2dc
Erwandy Lim <erwandy.dev@gmail.com>
2026-02-01 19:19:06 +0800

    SRP Solution: Split out Calculate and Print (Info) classes to handle its own responsibilities

---
+++ solid-net-sample/MathShapeCalc/ShapesInfo.cs
@@ -0,0 +1,12 @@
+public class ShapesInfo
+{
+    public String print_shapes_count(int shapes_count)
+    {
+        return "Total Shapes: " + shapes_count;
+    }
+
+    public String print_shapes_calculated_areas(double sum_up_all_areas)
+    {
+        return "Total Area: " + sum_up_all_areas;
+    }
+}
--- solid-net-sample/Program.cs
+++ solid-net-sample/Program.cs
@@ -6,6 +6,7 @@ public class Program
     public static void Main(string[] args)
     {
         CalculateArea areaCalculator = new CalculateArea();
+        
         Square square = new Square(5);
         Rectangle rectangle = new Rectangle(4, 6);
         Circle circle = new Circle(3);
@@ -13,7 +14,8 @@ public class Program
         List<Object> shapes = new List<Object> { square, rectangle, circle };
         double totalArea = areaCalculator.sum_up_all_areas(shapes);
 
-        Console.WriteLine(areaCalculator.print_shapes_count(shapes));
-        Console.WriteLine(areaCalculator.print_shapes_calculated_areas(shapes));
+        ShapesInfo shapesInfo = new ShapesInfo();
+        Console.WriteLine(shapesInfo.print_shapes_count(shapes.Count));
+        Console.WriteLine(shapesInfo.print_shapes_calculated_areas(totalArea));
     }
 }
